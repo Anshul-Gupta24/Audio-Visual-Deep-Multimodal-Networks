130157ccdcf7171ab4719318043f503cc15b0287
Anshul Gupta <anshul624.gupta@gmail.com>
2019-09-12 23:43:15 +0530

    Update and rename google_tts.py to ibm_tts.py

---
+++ Preprocessing/ibm_tts.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import pandas as pd
+
+
+words = open('../classes.txt','r').read().split('\n')
+words = words[:-1]
+
+voices = ['en-US_AllisonVoice', 'en-US_MichaelVoice', 'en-GB_KateVoice']
+
+for w in words:
+	c = 0
+	for v in voices:
+		print(w)
+		print()	
+		os.system(("bash /home/anshulg/WordNet/get_ibm_tts.sh"+" "+w+" "+v+" _"+str(c)).encode('utf-8'))
+		c += 1
