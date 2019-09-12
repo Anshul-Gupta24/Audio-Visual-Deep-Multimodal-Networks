899f1fa7e17a9af8551a665e7d2ac524f56d5120
Anshul Gupta <anshul624.gupta@gmail.com>
2019-09-13 00:03:54 +0530

    Update microsoft_tts.py

--- Preprocessing/microsoft_tts.py
+++ Preprocessing/microsoft_tts.py
@@ -1,21 +1,62 @@
-import os
+import os, requests, time
 import numpy as np
 import pandas as pd
-from get_tts import TextToSpeech
 import time
 import pandas as pd
+from xml.etree import ElementTree
+
+
+class TextToSpeech(object):
+    def __init__(self, subscription_key):
+        self.subscription_key = subscription_key
+        self.timestr = time.strftime("%Y%m%d-%H%M")
+        self.access_token = None
+
+
+    def get_token(self):
+        fetch_token_url = "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken"
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        self.access_token = str(response.text)
+
+
+    def save_audio(self, language, speaker, text, label, number):
+        base_url = 'https://westus.tts.speech.microsoft.com/'
+        path = 'cognitiveservices/v1'
+        constructed_url = base_url + path
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token,
+            'Content-Type': 'application/ssml+xml',
+            'X-Microsoft-OutputFormat': 'riff-16khz-16bit-mono-pcm',
+            'User-Agent': 'YOUR_RESOURCE_NAME'
+        }
+        xml_body = ElementTree.Element('speak', version='1.0')
+        xml_body.set('{http://www.w3.org/XML/1998/namespace}lang', language)
+        voice = ElementTree.SubElement(xml_body, 'voice')
+        voice.set('{http://www.w3.org/XML/1998/namespace}lang', language)
+        voice.set('name', 'Microsoft Server Speech Text to Speech Voice ('+language+', '+speaker+')')
+        voice.text = label
+        body = ElementTree.tostring(xml_body)
+
+        response = requests.post(constructed_url, headers=headers, data=body)
+        if response.status_code == 200:
+            with open('../Data/microsoft_synth_eng/synthesize-audio_' + label + '_' + str(number) + '.wav', 'wb') as audio:
+                audio.write(response.content)
+                print("\nStatus code: " + str(response.status_code) + "\nYour TTS is ready for playback.\n")
+        else:
+            print("\nStatus code: " + str(response.status_code) + "\nSomething went wrong. Check your subscription key and headers.\n")
 
 
 
-subscription_key = "27f7ef139ac44f049065ebdfe6f67762"
+subscription_key = "<enter_your_subscription_key>"
 app = TextToSpeech(subscription_key)
 app.get_token()
 
 
-labels = open('/home/anshulg/WordNet/labels_removed2.txt','r').read().split('\n')
+labels = open('../classes.txt','r').read().split('\n')
 labels = labels[:-1]
-
-#print labels
 
 
 accents = ['en-US', 'en-IN','en-AU', 'en-CA', 'en-GB', 'en-IE']
@@ -33,86 +74,24 @@ voices['en-IE'] = ['Sean']
 # voices['ja-JP'] = ['Ayumi, Apollo', 'Ichiro, Apollo', 'HarukaRUS']
 
 
-
-# df = pd.read_csv('/home/anshulg/WordNet/experiment_labels.csv')
-# katas = df['kata'].values
-# katakana_eng = df['eng'].values[katas=='katakana']
-# katakana = df['jap'].values[katas=='katakana']
-
-# hiragana_eng = df['eng'].values[katas=='hiragana']
-# hiragana = df['jap'].values[katas=='hiragana']
-
-# eng = np.concatenate([katakana_eng, hiragana_eng])
-# jap = np.concatenate([katakana, hiragana])
-
-# done = os.listdir('/home/data1/anshulg/microsoft_synth_jap_exp')
-# count = 0
-# for i in range(len(eng)):
-#     if 'synthesize-audio_'+eng[i]+'_0.wav' not in done:
-#         c = 0
-#         print(eng[i])
-#         for a in accents:
-#             for v in voices[a]:      
-#                 print(v)      
-#                 print(c)
-#                 app.save_audio(a, v, jap[i], eng[i], c)
-#                 c += 1
-#                 count +=1
-#                 time.sleep(3)
-
-#         if(count%195==0):
-#             # time.sleep(5)
-#             print()
-#             print('new token!!!')
-#             print()
-#             app = TextToSpeech(subscription_key)
-#             app.get_token()
-
-
-#         print()
-
-# words = ['screen', 'map', 'ship', 'crown', 'swan', 'shoes', 'socks']
-# words_inds = [i for i in range(len(eng)) if eng[i] in words]
-
-# for i in words_inds:
-#     print(eng[i])
-#     c = 0
-#     for a in accents:
-#         for v in voices[a]:  
-#             print(v)      
-#             print(c)
-#             app.save_audio(a, v, jap[i], eng[i], c)
-#             c += 1
-#             time.sleep(3)
-
-# words = open('/home/anshulg/WordNet/labels_exp2.txt','r').read().split('\n')
-# words = words[:-1]
-words = ['books', 'chair']
-
-
+done = os.listdir('../Data/microsoft_synth_eng')
 count = 0
-for w in words:
-    # if 'synthesize-audio_'+eng[i]+'_0.wav' not in done:
+for i in range(len(eng)):
+    if 'synthesize-audio_'+eng[i]+'_0.wav' not in done:
         c = 0
-        print(w)
+        print(eng[i])
         for a in accents:
             for v in voices[a]:      
                 print(v)      
                 print(c)
-                app.save_audio(a, v, '', w, c)
+                app.save_audio(a, v, jap[i], eng[i], c)
                 c += 1
                 count +=1
                 time.sleep(3)
 
         if(count%195==0):
-            # time.sleep(5)
             print()
             print('new token!!!')
             print()
             app = TextToSpeech(subscription_key)
             app.get_token()
-
-
-        print()
-
-
