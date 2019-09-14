1a88441ca1c86a2d7f122e50f5ededd87c0f1826
Anshul Gupta <anshul624.gupta@gmail.com>
2019-09-14 15:21:13 +0530

    Update ibm_tts.py

--- Preprocessing/ibm_tts.py
+++ Preprocessing/ibm_tts.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-
+'''
+Script to generate IBM TTS data
+'''
 import os
 import numpy as np
 import pandas as pd
@@ -8,7 +10,10 @@ import pandas as pd
 words = open('../classes.txt','r').read().split('\n')
 words = words[:-1]
 
+# English voices
 voices = ['en-US_AllisonVoice', 'en-US_MichaelVoice', 'en-GB_KateVoice']
+# Japanese voices
+#voices = ['ja-JP_EmiVoice']
 
 for w in words:
 	c = 0
