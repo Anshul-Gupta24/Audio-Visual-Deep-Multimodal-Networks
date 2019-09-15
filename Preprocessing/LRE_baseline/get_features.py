ca482294892d462153e1de49ae30aa3b9de45d7b
Anshul Gupta <anshul624.gupta@gmail.com>
2019-09-15 18:18:36 +0530

    Update get_features.py

--- Preprocessing/LRE_baseline/get_features.py
+++ Preprocessing/LRE_baseline/get_features.py
@@ -30,9 +30,9 @@ lre17_system = LRE17System(config)
 base_path = '../../Data/
 locs = ['google_synth_eng', 'ibm_synth_eng', 'microsoft_synth_eng', 'google_synth_jap', 'ibm_synth_jap', 'microsoft_synth_jap']
 pkls = ['google_synth_features.pkl', 'ibm_synth_features.pkl', 'microsoft_synth_features.pkl', 'google_synth_features_jap.pkl', 'ibm_synth_features_jap.pkl', 'microsoft_synth_features_jap.pkl'] 
-features = {}
 
 for j, loc in enumerate(locs):
+	features = {}
 	for i, w in enumerate(os.listdir(base_path+loc)):
 		if(w[-3:]=='wav'):
 			print(w)
