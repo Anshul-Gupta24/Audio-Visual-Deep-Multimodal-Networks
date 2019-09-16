d37e858dff866d1b24d8b911ab7647df47237d12
Anshul Gupta <anshul624.gupta@gmail.com>
2019-09-17 00:03:51 +0530

    Update preprocessing.sh

--- Preprocessing/preprocessing.sh
+++ Preprocessing/preprocessing.sh
@@ -1,5 +1,6 @@
-mkdir Saved_models
-mkdir Plots
+mkdir ../Saved_models
+mkdir ../Plots
 python LRE_baseline/get_features.py
 python get_speech_features_seq.py
+python get_speech_features_seq_jap.py
 python create_csv.py
