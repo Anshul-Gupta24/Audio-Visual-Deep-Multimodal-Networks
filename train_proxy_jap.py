20a44603e800f89f76d134b10a410374572954d1
Anshul Gupta <anshul624.gupta@gmail.com>
2019-09-22 18:49:57 +0530

    Update train_proxy_jap.py

--- train_proxy_jap.py
+++ train_proxy_jap.py
@@ -1,7 +1,6 @@
 '''
-Code to train the proxy based deep multimodal network for English speech-image input
+Code to train the proxy based deep multimodal network for Japanese speech-image input
 '''
-
 from model_proxy import JointNet
 import keras	
 import numpy as np
@@ -24,10 +23,18 @@ os.environ['HDF5_USE_FILE_LOCKING']='FALSE'
 if __name__ == '__main__':
 
     start = timeit.default_timer()
-    BATCH_SIZE = 128
+    BATCH_SIZE = 5
     AUD_FEAT = 2048
     IMG_FEAT = 2048
-    NUM_CLASSES = 655
+    NUM_CLASSES = 90
+    num_samples_train = 15
+    num_samples_val = 55
+
+
+    jointnet = JointNet(np.zeros((576, NUM_CLASSES)))
+    model = jointnet.model
+    aud_transform = jointnet.audio_submodel
+    model.load_weights('Saved_models/model_proxy/saved-model-24.hdf5', by_name=True)    # Choose epoch weights to load
 
 
     with open('Data/speech_features_2048D_jap.pkl', 'rb') as fp:
@@ -40,133 +47,101 @@ if __name__ == '__main__':
         img_data_val = pickle.load(fp)
 
 
-    classes = open('classes.txt','r').read().split('\n')
-    classes = classes[:-1]
-    label_ind = {c:i for i,c in enumerate(classes)}
+    new_classes = open('new_classes.txt','r').read().split('\n')
+    new_classes = new_classes[:-1]
+    label_ind_new = {c:i for i,c in enumerate(new_classes)}
 
 
-    df_train_aud = pd.read_csv('Data/train_data_proxy_audio_jap.csv')
+    df_train = pd.read_csv('Data/train_data_proxy_audio_jap.csv')
     df_val = pd.read_csv('Data/val_data_proxy_jap.csv')
-
     
-    def generator_uni(df, img_data, grd):
+    
+    def generator(df, img_data, i, num_samples):
         
-        num_samples = df.shape[0]
-        i = 0
-        while True:
+        groundings = df['0'].values[i:i+num_samples]
+        anchor_labels = df['1'].values[i:i+num_samples]
+        anchor_indices = df['2'].values[i:i+num_samples]
+        
 
-            if i%num_samples>=0:
-                df = df.sample(frac=1).reset_index(drop=True)                
-                i = 0
+        anchor_img = []
+        class_mask = np.zeros((num_samples, NUM_CLASSES))
+        class_mask_bar = np.ones((num_samples, NUM_CLASSES))
+        anchor_aud = []
+        for s,x in enumerate(groundings):                
+            label = anchor_labels[s]
+            class_mask[s][label_ind_new[label]] = 1
+            class_mask_bar[s][label_ind_new[label]] = 0
+
+            # image anchor
+            if x==0:                    
+                anchor_img.append(img_data[label][anchor_indices[s]])
+                anchor_aud.append(np.zeros(AUD_FEAT))
+                # anchor_aud.append(np.zeros((max_len, AUD_FEAT)))                
                 
-        
-            if grd==0:
-                groundings = np.zeros(BATCH_SIZE)
-            else:
-                groundings = np.ones(BATCH_SIZE)
-            anchor_labels = df['0'].values[i:i+BATCH_SIZE]
-            anchor_indices = df['1'].values[i:i+BATCH_SIZE]
-            
-
-            anchor_img = []
-            class_mask = np.zeros((BATCH_SIZE, NUM_CLASSES))
-            class_mask_bar = np.ones((BATCH_SIZE, NUM_CLASSES))
-            anchor_aud = []
-            for s,x in enumerate(groundings):                
-                label = anchor_labels[s]
-                class_mask[s][label_ind_new[label]] = 1
-                class_mask_bar[s][label_ind_new[label]] = 0
-
-                # image anchor
-                if x==0:                    
-                    anchor_img.append(img_data[label][anchor_indices[s]])
-                    anchor_aud.append(np.zeros(AUD_FEAT))
-                   
-                    
-                # audio anchor
-                else:                   
-                    anchor_aud.append(speech_data[label][anchor_indices[s]])
-                    anchor_img.append(np.zeros(IMG_FEAT))
+            # audio anchor
+            else:                   
+                anchor_aud.append(speech_data[label][anchor_indices[s]])
+                anchor_img.append(np.zeros(IMG_FEAT))
 
                                
-            yield [groundings, 1-groundings, np.array(anchor_aud), np.array(anchor_img), np.array(class_mask), np.array(class_mask_bar)], np.zeros(BATCH_SIZE)
-            
-            i += BATCH_SIZE
-    
+        return [groundings, 1-groundings, np.array(anchor_aud), np.array(anchor_img), np.array(class_mask), np.array(class_mask_bar)], np.zeros(num_samples)
 
-    def generator(df, img_data):
+
+
+    def generator_uni(df, img_data, i, num_samples):
         
-        num_samples = df.shape[0]
-        i = 0
-        while True:
+        groundings = np.ones(num_samples)
+        anchor_labels = df['0'].values[i:i+num_samples]
+        anchor_indices = df['1'].values[i:i+num_samples]
+        
 
-            if i%num_samples>=0:
-                df = df.sample(frac=1).reset_index(drop=True)                
-                i = 0
+        anchor_img = []
+        class_mask = np.zeros((num_samples, NUM_CLASSES))
+        class_mask_bar = np.ones((num_samples, NUM_CLASSES))
+        anchor_aud = []
+        for s,x in enumerate(groundings):                
+            label = anchor_labels[s]
+            class_mask[s][label_ind_new[label]] = 1
+            class_mask_bar[s][label_ind_new[label]] = 0
+
+            # image anchor
+            if x==0:                    
+                anchor_img.append(img_data[label][anchor_indices[s]])
+                anchor_aud.append(np.zeros(AUD_FEAT))
                 
-
-            groundings = df['0'].values[i:i+BATCH_SIZE]
-            anchor_labels = df['1'].values[i:i+BATCH_SIZE]
-            anchor_indices = df['2'].values[i:i+BATCH_SIZE]
-            
-
-            anchor_img = []
-            class_mask = np.zeros((BATCH_SIZE, NUM_CLASSES))
-            class_mask_bar = np.ones((BATCH_SIZE, NUM_CLASSES))
-            anchor_aud = []
-            for s,x in enumerate(groundings):                
-                label = anchor_labels[s]
-                class_mask[s][label_ind_new[label]] = 1
-                class_mask_bar[s][label_ind_new[label]] = 0
-
-                # image anchor
-                if x==0:                    
-                    anchor_img.append(img_data[label][anchor_indices[s]])
-                    anchor_aud.append(np.zeros(AUD_FEAT))
-                   
-                    
-                # audio anchor
-                else:                   
-                    anchor_aud.append(speech_data[label][anchor_indices[s]])
-                    anchor_img.append(np.zeros(IMG_FEAT))
+                
+            # audio anchor
+            else:                   
+                anchor_aud.append(speech_data[label][anchor_indices[s]])
+                anchor_img.append(np.zeros(IMG_FEAT))
 
                                
-            yield [groundings, 1-groundings, np.array(anchor_aud), np.array(anchor_img), np.array(class_mask), np.array(class_mask_bar)], np.zeros(BATCH_SIZE)
-            
-            i += BATCH_SIZE
+        return [groundings, 1-groundings, np.array(anchor_aud), np.array(anchor_img), np.array(class_mask), np.array(class_mask_bar)], np.zeros(num_samples)
 
 
-   
-    num_train_batches = ((df_train.shape[0]) / BATCH_SIZE)
-    num_val_batches = ((df_val.shape[0]) / BATCH_SIZE)
+    i = 0
+    j = 0
+    for x in range(18):
+
+        model.load_weights('Saved_models/model_proxy/saved-model-24.hdf5', by_name=True)    # For fresh start
+        model.compile(loss=jointnet.identity_loss, optimizer=keras.optimizers.Adam(lr=0.001, decay=1e-5))
+
+        folder = "Saved_models/model_proxy_jap/block_"+str(x)
+        if not os.path.isdir(folder):
+            os.system('mkdir '+folder)
+        filepath = folder+"/saved-model-{epoch:02d}.hdf5"
+        checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=True, mode='min', period=1)
+        callbacks_list = [checkpoint]
+
+
+        num_train_batches = ((df_train.shape[0]) / BATCH_SIZE)
+        num_val_batches = ((df_val.shape[0]) / BATCH_SIZE)
+
+        X, Y = generator_uni(df_train, flickr_train, i, num_samples_train)
+        X_val, Y_val = generator(df_val, flickr_val, i, num_samples_val)
+
+        history = model.fit(X, Y, batch_size=BATCH_SIZE, epochs=20, callbacks=callbacks_list, validation_data=(X_val, Y_val), shuffle=True)
+
+        i += num_samples_train
+        j += num_samples_val
     
-    folder = "Saved_models/model_proxy_jap"
-    if not os.path.isdir(folder):
-        os.system('mkdir '+folder)
-    filepath = folder+"/saved-model-{epoch:02d}.hdf5"
-    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, save_weights_only=True, period=1)
-    callbacks_list = [checkpoint]
-    
-    
-    jointnet = JointNet(np.zeros((576, NUM_CLASSES)), False)
-    model = jointnet.model
-    model.load_weights('Saved_models/model_proxy/saved-model-24.hdf5', by_name=True)    # Choose epoch weights to load
-
-    # Phase2: Train only on audio
-    # Train in batches of 5
-    blocks = []
-    for b in blocks:
-        
-        # model.load_weights('Saved_models/model_proxy/saved-model-24.hdf5', by_name=True)    load weights for fresh start
-        history = model.fit_generator(generator_uni(df_train_aud, img_data_train, 1), steps_per_epoch=num_train_batches, epochs=100, callbacks=callbacks_list, validation_data=generator(df_val, img_data_val), initial_epoch=0, validation_steps=num_val_batches)
-
-
-    print(history.history.keys())
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.legend(['train', 'val'], loc='upper left')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.savefig("Plots/model_proxy_loss_jap.png", bbox_inches = 'tight', pad_inches = 0)
-    plt.close()
-   
