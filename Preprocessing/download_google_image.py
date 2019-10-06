ce31590b4f387f72d6a9def8e3a38184be4eeb19
Anshul Gupta <anshul624.gupta@gmail.com>
2019-10-06 15:56:43 +0530

    Update download_google_image.py

--- Preprocessing/download_google_image.py
+++ Preprocessing/download_google_image.py
@@ -1,3 +1,5 @@
+# First install google_images_download
+# pip install google_images_download
 from google_images_download import google_images_download
 from functions.data import get_label_to_remap
 import pandas as pd
@@ -7,13 +9,11 @@ output_directory = 'Google_images'
 
 def download(keyword, imgdir):
     image_directory = imgdir
-    # '/home/kiranp/conda/bin/chromedriver'
-    arguments = {'keywords':keyword, 'limit':150,'print_urls':False,'image_directory':image_directory, 'output_directory':output_directory, 'chromedriver':'/home/kiran/Downloads/chromedriver'}   #creating list of arguments
+    arguments = {'keywords':keyword, 'limit':150,'print_urls':False,'image_directory':image_directory, 'output_directory':output_directory, 'chromedriver':'<your_path>'}   #creating list of arguments
     paths = response.download(arguments)   #passing the arguments to the function
     print(paths)   #printing absolute paths of the downloaded images
 
-labels = pd.read_csv('data/experiment_labels.csv')
-keywords = sorted(labels.values[:, 0])
+labels = open('../new_classes.txt').read().split('\n')[:-1]
 l2r = get_label_to_remap()
 
 for keyword in keywords:
