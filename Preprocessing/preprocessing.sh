1bdab1aba72c2029ef790f2effd5517da91584ae
Anshul Gupta <anshul624.gupta@gmail.com>
2019-10-10 23:22:14 +0530

    Update preprocessing.sh

--- Preprocessing/preprocessing.sh
+++ Preprocessing/preprocessing.sh
@@ -3,5 +3,6 @@ mkdir ../Plots
 python LRE_baseline/get_features.py
 python get_speech_features_seq.py
 python get_speech_features_seq_jap.py
+python get_image_features_2048D.py
 python create_csv.py
 python create_csv_jap.py
